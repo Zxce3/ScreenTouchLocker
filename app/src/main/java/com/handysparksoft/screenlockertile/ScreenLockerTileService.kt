7f453cc44ee00c1945ee8bc8f340b3f2b30faaee
David Asensio <david.asensio@xing.com>
2022-03-21 12:35:39 +0100

    ScreenLockerTile Service and icon

---
+++ app/src/main/java/com/handysparksoft/screenlockertile/ScreenLockerTileService.kt
@@ -0,0 +1,27 @@
+package com.handysparksoft.screenlockertile
+
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+
+class ScreenLockerTileService : TileService() {
+
+    override fun onTileAdded() {
+        super.onTileAdded()
+        updateTileState(true)
+    }
+
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+    }
+
+    override fun onClick() {
+        super.onClick()
+        val activeState = qsTile.state == Tile.STATE_ACTIVE
+        updateTileState(!activeState)
+    }
+
+    private fun updateTileState(activeState: Boolean) {
+        qsTile.state = if (activeState) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+        qsTile.updateTile()
+    }
+}
